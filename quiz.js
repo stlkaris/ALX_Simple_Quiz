a00791757cda32140de835fa789a7fd665753b05
Stella Kariuki <stlkariuki4@gmail.com>
2024-07-13 01:33:37 +0300

    index.html

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer () {
+     const correctAnswer = "4";
+
+     const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
+      
+     const userAnswer = selectedAnswer ? selectedAnswer.value : null;
+     
+     if(userAnswer === correctAnswer) {
+        document.getElementById('feedback').textContent = 'Correct! well done.';
+     } else {
+        document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+     }
+
+}
+
+document.getElementById("submit-answer").addEventListener('click', checkAnswer);
