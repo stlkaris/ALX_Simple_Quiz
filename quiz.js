74bc7b2bcd66d3a87d3308b4f8e1c02a91c9e566
Stella Kariuki <stlkariuki4@gmail.com>
2024-07-13 02:46:47 +0300

    calculator.html

--- quiz.js
+++ quiz.js
@@ -13,4 +13,4 @@ function checkAnswer () {
 
 }
 
-document.getElementById("submit-answer").addEventListener('click', checkAnswer);
+document.getElementById("submit-answer").addEventListener('click', checkAnswer)
